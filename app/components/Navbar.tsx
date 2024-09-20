955faa99468b2b8791ce2af79827abd96493a9ad
John Michael Cyril Landicho <johnmichaelcyrillandicho@Johns-MBP.homenet.telecomitalia.it>
2024-09-20 20:45:50 +0200

    created parallax effect for the homepage

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="flex bg-white justify-between py-6">
+        <div className="text-black">ciao</div>
+        <div className="text-black">ciao</div>
+        <div className="text-black">ciao</div>
+    </div>
+  )
+}
+
+export default Navbar
