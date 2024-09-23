5ffa69e05024cb04e1e587a04de74525bc42df85
Ethan <ethanbone435@gmail.com>
2024-09-23 14:29:22 +0200

    moved hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,14 @@
-"use client";
 
-import { useEffect, useState } from "react";
+import React from "react";
+import Hero from "./components/Hero";
 import "./homepage.css";
 
 export default function Home() {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    const handleMouseMove = (event: { clientX: number; clientY: number }) => {
-      const x = (event.clientX - window.innerWidth / 2) / window.innerWidth;
-      const y = (event.clientY - window.innerHeight / 2) / window.innerHeight;
-      setMousePosition({ x, y });
-    };
-
-    window.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
 
   return (
-    <>
-      <section>
-        <div
-          className="background"
-          style={{
-            transform: `translate(${mousePosition.x * 10}px, ${mousePosition.y * 10}px)`,
-            transition: "transform 0.1s ease-out",
-          }}
-        >
-          <div
-            className="absolute left-0 top-40 text-black left-rectangle"
-            style={{
-              transform: `translate(${mousePosition.x * 10}px, ${mousePosition.y * 10}px)`,
-              transition: "transform 0.1s ease-out",
-            }}
-          ></div>
-          <div
-            className="absolute right-0 top-60 text-black right-rectangle"
-            style={{
-              transform: `translate(${mousePosition.x * 10}px, ${mousePosition.y * 10}px)`,
-              transition: "transform 0.1s ease-out",
-            }}
-          ></div>
-          <h1
-            className="flex items-center justify-center h-screen text-black"
-            style={{
-              transform: `translate(${mousePosition.x * 10}px, ${mousePosition.y * 10}px)`,
-              transition: "transform 0.1s ease-out",
-            }}
-          >
-            Prometheus
-          </h1>
-        </div>
-      </section>
-    </>
+    <React.Fragment>
+      <Hero/>
+    </React.Fragment>
   );
 }
