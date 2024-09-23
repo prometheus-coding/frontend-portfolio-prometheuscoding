dd549097363d7f14818909f796ab0282593ae7d4
Ethan <ethanbone435@gmail.com>
2024-09-23 18:07:54 +0200

    init hero

---
+++ app/components/Hero2.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero2 = () => {
+  return (
+    <div className='w-full border-blue-800 border-2 grid place-items-center h-[600px]'>
+      <div className='border-green-400 border-2 w-auto h-full flex items-center'>
+        <h1 className='text-gray-900'>We create things that live on the internet</h1>
+        <div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero2
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import Hero from "./components/Hero";
+// import Hero from "./components/Hero";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import "./homepage.css";
+import Hero2 from "./components/Hero2";
 
 export default function Home() {
   return (
     <React.Fragment>
       <Header />
+      <Hero2/>
       <Navbar />
     </React.Fragment>
   );
