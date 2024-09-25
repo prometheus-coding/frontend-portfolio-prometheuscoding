a1d08718d8f92e13d326ed5be8be4c2a6c03c6ad
MindfulLearner <dimajosh1009@gmail.com>
2024-09-25 13:59:19 +0000

    aggiungi componenti

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import "./homepage.css";
 import Hero2 from "./components/Hero2";
+import Section1 from "./components/Section1";
 
 export default function Home() {
   return (
     <React.Fragment>
       <Header />
-      <Hero2/>
+      <Hero2 />
+      <Section1 />
       <Navbar />
     </React.Fragment>
   );
