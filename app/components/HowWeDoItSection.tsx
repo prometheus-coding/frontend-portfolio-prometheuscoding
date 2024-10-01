7e6ba42931428728cc51f5255db56b482eab6a32
MindfulLearner <dimajosh1009@gmail.com>
2024-10-01 23:41:35 +0000

    sezione creazione del sito con noi, usato grid colorati mancano alcuni componentistiche

--- app/components/HowWeDoItSection.tsx
+++ app/components/HowWeDoItSection.tsx
@@ -1,65 +1,23 @@
+import { div } from "framer-motion/client";
+
 const HowWeDoItSection = () => {
   return (
-    <div className="h-screen">
-      <div className="max-w-[1270px] p-4 mx-auto">
-        <div className="flex justify-center items-center">
-          <h1 className="text-5xl font-valentino text-gray-900 text-center">
-            Let's Connect!
-          </h1>
+    <div className="h-screen centered content-center">
+      <div className="grid grid-rows-12 grid-cols-12 gap-4 h-[768px]">
+        <div className="row-span-4 col-span-7 bg-blue-300 p-4">1. Meeting</div>
+        <div className="row-span-4 col-span-5 bg-green-300 p-4">2. Design</div>
+        <div className="row-span-4 col-span-5 bg-yellow-300 p-4">
+          3. Development
         </div>
-      </div>
-
-      <div className="flex flex-col md:flex-row lg:flex-row flex-wrap gap-8 mt-8 max-w-[1270px] mx-auto p-4">
-        <div className="bg-gray-800 text-white rounded-lg p-6 shadow-lg flex-1">
-          <h3 className="text-xl font-bold mb-4">PLANNING</h3>
-          <p className="mb-4">
-            We collaborate with our clients to understand their vision and
-            define a clear plan.
-          </p>
-          <div className="flex items-center space-x-2">
-            <span>Zoom, Task, Presentation</span>
-          </div>
+        <div className="row-span-4 col-span-4 bg-orange-300 p-4">
+          4. Maintenance
         </div>
-
-        <div className="bg-gray-800 text-white rounded-lg p-6 shadow-lg flex-1">
-          <h3 className="text-xl font-bold mb-4">DESIGN</h3>
-          <p className="mb-4">
-            We transform the concept into an initial design, forming the basis
-            of the project.
-          </p>
-        </div>
-
-        <div className="bg-gray-800 text-white rounded-lg p-6 shadow-lg flex-1">
-          <h3 className="text-xl font-bold mb-4">DEVELOPMENT</h3>
-          <p className="mb-4">
-            We use the latest technologies to develop a high-quality product.
-          </p>
-        </div>
-
-        <div className="bg-gray-800 text-white rounded-lg p-6 shadow-lg flex-1">
-          <h3 className="text-xl font-bold mb-4">MAINTENANCE</h3>
-          <p className="mb-4">
-            We ensure quality through ongoing monitoring and maintenance.
-          </p>
-        </div>
-
-        <div className="bg-gray-800 text-white rounded-lg p-6 shadow-lg flex-1">
-          <h3 className="text-xl font-bold mb-4">LAUNCH</h3>
-          <p className="mb-4">
-            We manage websites launch and deployment for a seamless go-live.
-          </p>
-        </div>
-
-        <div className="bg-gray-800 text-white rounded-lg p-6 shadow-lg flex-1">
-          <h3 className="text-xl font-bold mb-4">Not convinced yet?</h3>
-          <p className="mb-4">
-            We are ready to provide you with more information if you need to
-            learn more about us.
-          </p>
+        <div className="row-span-8 col-span-3 bg-red-300 p-4">5. Launch</div>
+        <div className="row-span-4 col-span-9 bg-purple-300 p-4">
+          6. Not convinced yet?
         </div>
       </div>
     </div>
   );
 };
-
 export default HowWeDoItSection;
