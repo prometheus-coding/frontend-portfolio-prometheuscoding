fde71c7e04b4d2bbe718e44ce89fc020560f030a
MindfulLearner <dimajosh1009@gmail.com>
2024-10-02 23:37:27 +0000

    responsive how we do it nad contact us updated, howwedoitsection gotta redo

--- app/components/ContactUsSection/ContactUsSection.tsx
+++ app/components/ContactUsSection/ContactUsSection.tsx
@@ -4,38 +4,40 @@ const ContactUsSection = () => {
   return (
     <Section sectionHeight="h-screen" bgColor="bg-primary-200">
       <div className="centered">
-        <div className="centered pb-3"> Let&apos;s have our first meeting </div>
-        <div className="flex">
-          <div className="w-7/12">
-            <div className="flex gap-4 mb-4">
+        <div className="pb-3 text-2xl w-full sm:w-4/12">
+          Let&apos;s have our first meeting
+        </div>
+        <div className="flex flex-col sm:flex-row">
+          <div className="w-full sm:w-7/12">
+            <div className="flex flex-wrap sm:flex-nowrap gap-4 mb-4">
               <input
                 type="text"
                 name="firstName"
                 placeholder="First Name"
-                className="p-3 w-6/12 outline outline-1 outline-white/50 bg-transparent rounded-lg"
+                className="p-3 w-full sm:w-6/12 outline outline-1 outline-white/50 bg-transparent rounded-lg"
               />
               <input
                 type="text"
                 name="lastName"
                 placeholder="Last Name"
-                className="p-3 w-6/12 outline outline-1 outline-white/50 bg-transparent rounded-lg"
+                className="p-3 w-full sm:w-6/12 outline outline-1 outline-white/50 bg-transparent rounded-lg"
               />
             </div>
-            <div className="flex gap-4 mb-4">
+            <div className="flex flex-wrap sm:flex-nowrap gap-4 mb-4">
               <input
                 type="tel"
                 name="phoneNumber"
                 placeholder="Phone Number"
-                className="p-3 w-6/12 outline outline-1 outline-white/50 bg-transparent rounded-lg"
+                className="p-3 w-full sm:w-6/12 outline outline-1 outline-white/50 bg-transparent rounded-lg"
               />
               <input
                 type="email"
                 name="email"
                 placeholder="Email"
-                className="p-3 w-6/12 outline outline-1 outline-white/50 bg-transparent rounded-lg"
+                className="p-3 w-full sm:w-6/12 outline outline-1 outline-white/50 bg-transparent rounded-lg"
               />
             </div>
-            <div className="flex gap-4 mb-4">
+            <div className="flex flex-wrap sm:flex-nowrap gap-4 mb-4">
               <input
                 type="text"
                 name="company"
@@ -43,21 +45,21 @@ const ContactUsSection = () => {
                 className="p-3 w-full outline outline-1 outline-white/50 bg-transparent rounded-lg"
               />
             </div>
-            <div className="flex gap-4 mb-4">
+            <div className="flex flex-wrap gap-4 mb-4">
               <textarea
                 name="message"
                 placeholder="Your Message 0/100"
                 className="p-3 w-full outline outline-1 outline-white/50 bg-transparent rounded-lg h-64"
               />
             </div>
-            <div className="flex w-[100px] gap-4 mb-4">
-              <button className="p-3 w-full bg-blue-500 text-white rounded-lg outline outline-1 outline-white/50">
+            <div className="flex gap-4 mb-4">
+              <button className="p-3 w-full sm:w-[100px] bg-blue-500 text-white rounded-lg outline outline-1 outline-white/50">
                 Submit
               </button>
             </div>
           </div>
-          <div className="w-5/12 flex justify-center items-center">
-            <div className="w-52">
+          <div className="w-full sm:w-5/12 flex justify-end items-center mt-6 sm:mt-0">
+            <div className="w-full sm:w-52">
               <div className="text-3xl">Quotes</div>
               <div>"He who has overcome his fears will truly be free"</div>
               <div>- Aristotle</div>
--- app/components/HowWeDoItSection/HowWeDoItSection.tsx
+++ app/components/HowWeDoItSection/HowWeDoItSection.tsx
@@ -3,21 +3,28 @@ import Section from "../common/Section";
 const HowWeDoItSection = () => {
   return (
     <Section sectionHeight="h-screen">
-      <div className="grid centered grid-rows-12 grid-cols-12 gap-4 h-[768px]">
-        <div className="row-span-4 col-span-7 bg-blue-300 p-4">1. Meeting</div>
-        <div className="row-span-4 col-span-5 bg-green-300 p-4">2. Design</div>
-        <div className="row-span-4 col-span-5 bg-yellow-300 p-4">
+      <div className="grid centered gap-4 h-[768px] sm:grid-rows-6 sm:grid-cols-6 md:grid-rows-12 md:grid-cols-12">
+        <div className="sm:row-span-1 sm:col-span-6 md:row-span-4 md:col-span-7 bg-blue-300 p-4">
+          1. Meeting
+        </div>
+        <div className="sm:row-span-1 sm:col-span-6 md:row-span-4 md:col-span-5 bg-green-300 p-4">
+          2. Design
+        </div>
+        <div className="sm:row-span-1 sm:col-span-6 md:row-span-4 md:col-span-5 bg-yellow-300 p-4">
           3. Development
         </div>
-        <div className="row-span-4 col-span-4 bg-orange-300 p-4">
+        <div className="sm:row-span-1 sm:col-span-6 md:row-span-4 md:col-span-4 bg-orange-300 p-4">
           4. Maintenance
         </div>
-        <div className="row-span-8 col-span-3 bg-red-300 p-4">5. Launch</div>
-        <div className="row-span-4 col-span-9 bg-purple-300 p-4">
+        <div className="sm:row-span-1 sm:col-span-6 md:row-span-8 md:col-span-3 bg-red-300 p-4">
+          5. Launch
+        </div>
+        <div className="sm:row-span-1 sm:col-span-6 md:row-span-4 md:col-span-9 bg-purple-300 p-4">
           6. Not convinced yet?
         </div>
       </div>
     </Section>
   );
 };
+
 export default HowWeDoItSection;
