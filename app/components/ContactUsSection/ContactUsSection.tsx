7cc0c463cf123b8c9741c1ff6361fe2292173f4d
MindfulLearner <dimajosh1009@gmail.com>
2024-10-02 20:43:39 +0000

    spostato componente

--- app/components/ContactUsSection/ContactUsSection.tsx
+++ app/components/ContactUsSection/ContactUsSection.tsx
@@ -7,60 +7,60 @@ const ContactUsSection = () => {
         <div className="centered pb-3"> Let&apos;s have our first meeting </div>
         <div className="flex">
           <div className="w-7/12">
-            <div className="flex h-[40px] gap-4 mb-4">
+            <div className="flex gap-4 mb-4">
               <input
                 type="text"
                 name="firstName"
                 placeholder="First Name"
-                className="h-full w-6/12 p-2 outline-black bg-transparent rounded-lg"
+                className="p-3 w-6/12 outline outline-1 outline-white/50 bg-transparent rounded-lg"
               />
               <input
                 type="text"
                 name="lastName"
                 placeholder="Last Name"
-                className="h-full w-6/12 p-2 outline-black bg-transparent rounded-lg"
+                className="p-3 w-6/12 outline outline-1 outline-white/50 bg-transparent rounded-lg"
               />
             </div>
-            <div className="flex h-[40px] gap-4 mb-4">
+            <div className="flex gap-4 mb-4">
               <input
                 type="tel"
                 name="phoneNumber"
                 placeholder="Phone Number"
-                className="h-full w-6/12 p-2 outline-black bg-transparent rounded-lg"
+                className="p-3 w-6/12 outline outline-1 outline-white/50 bg-transparent rounded-lg"
               />
               <input
                 type="email"
                 name="email"
                 placeholder="Email"
-                className="h-full w-6/12 p-2 outline-black bg-transparent rounded-lg"
+                className="p-3 w-6/12 outline outline-1 outline-white/50 bg-transparent rounded-lg"
               />
             </div>
-            <div className="flex h-[40px] gap-4 mb-4">
+            <div className="flex gap-4 mb-4">
               <input
                 type="text"
                 name="company"
                 placeholder="Company"
-                className="h-full w-full p-2 outline-black bg-transparent rounded-lg"
+                className="p-3 w-full outline outline-1 outline-white/50 bg-transparent rounded-lg"
               />
             </div>
-            <div className="flex h-[280px] gap-4 mb-4">
+            <div className="flex gap-4 mb-4">
               <textarea
                 name="message"
                 placeholder="Your Message 0/100"
-                className="h-full w-full p-2 outline-black bg-transparent rounded-lg"
+                className="p-3 w-full outline outline-1 outline-white/50 bg-transparent rounded-lg h-64"
               />
             </div>
-            <div className="flex w-[100px] h-[40px] gap-4 mb-4">
-              <button className="h-full w-full bg-blue-500 text-white rounded-lg outline-black">
+            <div className="flex w-[100px] gap-4 mb-4">
+              <button className="p-3 w-full bg-blue-500 text-white rounded-lg outline outline-1 outline-white/50">
                 Submit
               </button>
             </div>
           </div>
-          <div className="w-5/12">
-            <div className="w-[100px]">
-              <h2>Quotes</h2>
+          <div className="w-5/12 flex justify-center items-center">
+            <div className="w-52">
+              <div className="text-3xl">Quotes</div>
               <div>"He who has overcome his fears will truly be free"</div>
-              <div>- Aritotle</div>
+              <div>- Aristotle</div>
             </div>
           </div>
         </div>
