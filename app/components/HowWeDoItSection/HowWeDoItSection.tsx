e49d12aec08c4985b8a8fe9f83d359da8f81eb0d
Ethan <ethanbone435@gmail.com>
2024-10-04 00:19:35 +0200

    fixa tu

--- app/components/HowWeDoItSection/HowWeDoItSection.tsx
+++ app/components/HowWeDoItSection/HowWeDoItSection.tsx
@@ -3,23 +3,23 @@ import Section from "../common/Section";
 const HowWeDoItSection = () => {
   return (
     <Section>
-      <div className="p-2 grid centered gap-4 h-[768px] sm:grid-rows-6 sm:grid-cols-6 md:grid-rows-12 md:grid-cols-12">
-        <div className="sm:h-10 md:h-20 sm:row-span-1 sm:col-span-6 md:row-span-4 md:col-span-7 bg-blue-300 p-4">
+      <div className="p-2 grid centered gap-4 s:grid-rows-6 s:grid-cols-6 md:grid-rows-12 md:grid-cols-12">
+        <div className="s:row-span-1 s:col-span-6 h-[300px] md:row-span-4 md:col-span-7 bg-blue-300 p-4">
           1. Meeting
         </div>
-        <div className="sm:row-span-1 sm:col-span-6 md:row-span-4 md:col-span-5 bg-green-300 p-4">
+        <div className="s:row-span-1 s:col-span-6 h-[300px] md:row-span-4 md:col-span-5 bg-green-300 p-4">
           2. Design
         </div>
-        <div className="sm:row-span-1 sm:col-span-6 md:row-span-4 md:col-span-5 bg-yellow-300 p-4">
+        <div className="s:row-span-1 s:col-span-6 h-[300px] md:row-span-4 md:col-span-5 bg-yellow-300 p-4">
           3. Development
         </div>
-        <div className="sm:row-span-1 sm:col-span-6 md:row-span-4 md:col-span-4 bg-orange-300 p-4">
+        <div className="s:row-span-1 s:col-span-6 h-[300px] md:row-span-4 md:col-span-4 bg-orange-300 p-4">
           4. Maintenance
         </div>
-        <div className="sm:row-span-1 sm:col-span-6 md:row-span-8 md:col-span-3 bg-red-300 p-4">
+        <div className="s:row-span-1 s:col-span-6 h-[300px] md:h-auto md:row-span-8 md:col-span-3 bg-red-300 p-4">
           5. Launch
         </div>
-        <div className="sm:row-span-1 sm:col-span-6 md:row-span-4 md:col-span-9 bg-purple-300 p-4">
+        <div className="s:row-span-1 s:col-span-6 h-[300px] md:row-span-4 md:col-span-9 bg-purple-300 p-4">
           6. Not convinced yet?
         </div>
       </div>
