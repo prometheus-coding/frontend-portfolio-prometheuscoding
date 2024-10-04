fbdc2358e1e4b38da87c8881d8b5e83c92516ff4
Ethan <ethanbone435@gmail.com>
2024-10-04 18:12:56 +0200

    styling card test

--- app/components/ProjectSection/ProjectsCard.tsx
+++ app/components/ProjectSection/ProjectsCard.tsx
@@ -6,8 +6,9 @@ interface Props {
 
 const ProjectsCard = ({ imageUrl }: Props) => {
   return (
-    <div className="project-card shadow-md ~size-56/80">
-      <div className="h-full overflow-hidden rounded-md bg-cover bg-center" style={{backgroundImage:`url(${imageUrl})`}}>
+    <div className="relative project-card shadow-md ~size-56/80">
+      <div className="h-full overflow-hidden rounded-md bg-cover bg-center border-[1px] border-gray-700" style={{backgroundImage:`url(${imageUrl})`}}>
+      <div className="absolute h-full w-full bg-gradient-to-b from-transparent to-black opacity-50 hover:opacity-0 transition-opacity duration-200 ease-in-out"></div>
         {/* <img src={imageUrl} alt="image" className="" /> */}
       </div>
     </div>
