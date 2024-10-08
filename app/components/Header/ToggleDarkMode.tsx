1488cbde3813d06cc54bfd530496ec05cd839325
Ethan <ethanbone435@gmail.com>
2024-10-08 20:33:52 +0200

    fix alt

--- app/components/Header/ToggleDarkMode.tsx
+++ app/components/Header/ToggleDarkMode.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState } from "react";
+import Image from "next/image";
 const ToggleDarkMode = () => {
   const [darkMode, setDarkMode] = useState(false)
 
@@ -18,8 +19,7 @@ const ToggleDarkMode = () => {
           <div onClick={darkModeHandler} className="w-14 h-8 rounded-full outline outline-1 outline-black flex justify-start items-center pl-1">
             <div className="w-6 h-6 rounded-full outline outline-1 outline-black">
               <div className="p-1">
-                <img src="./headerAssets/467619.png" />
-
+                <Image src="./headerAssets/467619.png" alt="sun" />
               </div>
             </div>
           </div>
@@ -38,7 +38,7 @@ const ToggleDarkMode = () => {
               className="w-6 h-6 rounded-full outline outline-1 outline-white"
             >
               <div className="p-1">
-                <img src="./headerAssets/Moon-512.png" className="filter invert" />
+                <Image src="./headerAssets/Moon-512.png" alt="moon" className="filter invert" />
               </div>
             </div>
           </div>
